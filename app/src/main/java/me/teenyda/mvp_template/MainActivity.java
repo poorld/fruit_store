b0a5594c4d1f07449b38c742eaebcff3e6ef96ac
teenyda <teenyda@gmail.com>
2019-08-21 18:48:51 +0800

    状态栏更新

--- app/src/main/java/me/teenyda/mvp_template/MainActivity.java
+++ app/src/main/java/me/teenyda/mvp_template/MainActivity.java
@@ -1,7 +1,11 @@
 package me.teenyda.mvp_template;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import me.teenyda.mvp_template.model.test.StatusBarTestAct;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.to_test)
+                .setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent(MainActivity.this, StatusBarTestAct.class);
+                        startActivity(intent);
+                    }
+                });
     }
 }
--- app/src/main/java/me/teenyda/mvp_template/common/mvp/BaseActivity.java
+++ app/src/main/java/me/teenyda/mvp_template/common/mvp/BaseActivity.java
@@ -10,7 +10,10 @@ import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.RelativeLayout;
+
+import com.jaeger.library.StatusBarUtil;
 
 import me.teenyda.mvp_template.R;
 import me.teenyda.mvp_template.common.app.ActivityManager;
@@ -50,25 +53,45 @@ public abstract class BaseActivity extends AppCompatActivity {
         baseInitializer();
         registerBroadcart();
         mView = LayoutInflater.from(this).inflate(setR_layout(), null);
-
-        RelativeLayout startbarRL = mView.findViewById(R.id.start_bar);
-        if (startbarRL != null) {
-            int statusBarHeight = getStatusBarHeight();
-            startbarRL.setPadding(0,statusBarHeight,0, 0);
-        }
+        setContentView(mView);
+        setStatusBar(true);
         ActivityManager.getAppManager().addActivity(this);
         viewInitializer();
         doBuseness();
     }
 
-    private int getStatusBarHeight() {
-        int result = 0;
-        int resourceId = getResources().getIdentifier("status_bar_height", "dimen", "android");
-        if (resourceId > 0) {
-            result = getResources().getDimensionPixelSize(resourceId);
-        }
-        return result;
 
+    /**
+     * 状态栏设置
+     * @param padding
+     */
+    protected void setStatusBar(boolean padding) {
+        RelativeLayout startbarRL = mView.findViewById(R.id.start_bar);
+        if (padding && startbarRL != null) {
+            StatusBarUtil.setTranslucentForImageViewInFragment(this, 0, null);
+            ViewGroup.LayoutParams layoutParams = startbarRL.getLayoutParams();
+            layoutParams.height += getStatusBarHeight(this);
+            startbarRL.setLayoutParams(layoutParams);
+            View view = startbarRL.getChildAt(0);
+            ViewGroup.MarginLayoutParams params = (ViewGroup.MarginLayoutParams) view.getLayoutParams();
+            params.setMargins(0, getStatusBarHeight(this), 0, 0);
+
+        } else {
+            StatusBarUtil.setTranslucent(this);
+        }
+
+    }
+
+    /**
+     * 获取状态栏高度
+     *
+     * @param context context
+     * @return 状态栏高度
+     */
+    private static int getStatusBarHeight(Context context) {
+        // 获得状态栏高度
+        int resourceId = context.getResources().getIdentifier("status_bar_height", "dimen", "android");
+        return context.getResources().getDimensionPixelSize(resourceId);
     }
 
     protected abstract void baseInitializer();
