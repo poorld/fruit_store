91e79f78a1f5222027133da217239c8a9cc0d164
teenyda <teenyda@gmail.com>
2019-08-22 11:17:32 +0800

    添加缓存类

---
+++ app/src/main/java/me/teenyda/mvp_template/common/entity/UserToken.java
@@ -0,0 +1,31 @@
+package me.teenyda.mvp_template.common.entity;
+
+import java.io.Serializable;
+
+/**
+ * author: teenyda
+ * date: 2019/8/22
+ * description:
+ */
+public class UserToken implements Serializable {
+
+    private String userName;
+
+    private String token;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
--- app/src/test/java/me/teenyda/mvp_template/ExampleUnitTest.java
+++ app/src/test/java/me/teenyda/mvp_template/ExampleUnitTest.java
@@ -1,6 +1,10 @@
 package me.teenyda.mvp_template;
 
+import com.alibaba.fastjson.JSON;
+
 import org.junit.Test;
+
+import me.teenyda.mvp_template.common.entity.UserToken;
 
 import static org.junit.Assert.*;
 
@@ -13,5 +17,15 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
+    }
+
+
+    @Test
+    public void test1() {
+        UserToken userToken = new UserToken();
+        userToken.setUserName("teenyda");
+        userToken.setToken("fjdaskfjadks;fjadklsfjadisfweqwf");
+        String json = JSON.toJSONString(userToken);
+        System.out.println(json);
     }
 }
