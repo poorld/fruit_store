7099dabcae9da70647b2352be66e5a28e0363f0f
teenyda <teenyda@gmail.com>
2020-09-17 11:30:39 +0800

    购物车

--- app/src/main/java/me/teenyda/fruit_store/model/cart/base/ShoppingCartFragment.java
+++ app/src/main/java/me/teenyda/fruit_store/model/cart/base/ShoppingCartFragment.java
@@ -1,20 +1,27 @@
 package me.teenyda.fruit_store.model.cart.base;
 
 import android.content.Context;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.jaeger.library.StatusBarUtil;
 import com.jcodecraeer.xrecyclerview.XRecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import androidx.recyclerview.widget.LinearLayoutManager;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import me.teenyda.fruit_store.R;
+import me.teenyda.fruit_store.common.entity.Cart;
 import me.teenyda.fruit_store.common.mvp.MvpRxFragment;
+import me.teenyda.fruit_store.common.utils.ToolUtils;
 import me.teenyda.fruit_store.model.cart.base.adapter.ShoppingCartAdapter;
 import me.teenyda.fruit_store.model.cart.base.presenter.ShoppingCartPresenter;
 import me.teenyda.fruit_store.model.cart.base.view.IShoppingCartView;
-import me.teenyda.fruit_store.model.main.MainActivity;
 
 /**
  * author: teenyda
@@ -28,6 +35,14 @@ public class ShoppingCartFragment extends MvpRxFragment<IShoppingCartView, Shopp
 
     @BindView(R.id.start_bar)
     RelativeLayout start_bar;
+
+    @BindView(R.id.iv_cart_select_all)
+    ImageView iv_cart_select_all;
+
+    @BindView(R.id.tv_cart_total_price)
+    TextView tv_cart_total_price;
+
+    private ShoppingCartAdapter mShoppingCartAdapter;
 
 
     @Override
@@ -55,9 +70,41 @@ public class ShoppingCartFragment extends MvpRxFragment<IShoppingCartView, Shopp
 
         LinearLayoutManager manager = new LinearLayoutManager(getMContext());
         shopping_cart_rv.setLayoutManager(manager);
-        shopping_cart_rv.setAdapter(new ShoppingCartAdapter(getMContext()));
+        mShoppingCartAdapter = new ShoppingCartAdapter(getMContext());
+        shopping_cart_rv.setAdapter(mShoppingCartAdapter);
 
         shopping_cart_rv.setNestedScrollingEnabled(false);
+
+        List<Cart> carts = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            Cart cart = new Cart();
+            cart.setName("蜜柚9斤现摘宜昌青皮蜜橘新鲜水果当季孕妇橘子薄皮");
+            cart.setCount(ToolUtils.randomInt(5) + 1);
+            cart.setSpecification("规格: 水果一大箱");
+            cart.setPrice((float) ToolUtils.randomPrice(50));
+
+            carts.add(cart);
+        }
+
+        mShoppingCartAdapter.setCarts(carts);
+
+        mShoppingCartAdapter.setProductSelect(new ShoppingCartAdapter.IProductSelect() {
+            @Override
+            public void onProductedSelect(double price, boolean isSelectedAll) {
+                tv_cart_total_price.setText(String.format(getString(R.string.cart_total_price), price));
+                if (isSelectedAll != iv_cart_select_all.isSelected()) {
+                    iv_cart_select_all.setSelected(isSelectedAll);
+                }
+            }
+        });
+
+        iv_cart_select_all.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                iv_cart_select_all.setSelected(!iv_cart_select_all.isSelected());
+                mShoppingCartAdapter.selectedAll(iv_cart_select_all.isSelected());
+            }
+        });
 
     }
 
--- app/src/main/java/me/teenyda/fruit_store/model/cart/base/adapter/ShoppingCartAdapter.java
+++ app/src/main/java/me/teenyda/fruit_store/model/cart/base/adapter/ShoppingCartAdapter.java
@@ -4,11 +4,17 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.jcodecraeer.xrecyclerview.XRecyclerView;
 
+import java.util.List;
+
 import androidx.annotation.NonNull;
 import me.teenyda.fruit_store.R;
+import me.teenyda.fruit_store.common.entity.Cart;
+import me.teenyda.fruit_store.common.utils.ToolUtils;
 
 /**
  * author: teenyda
@@ -19,6 +25,39 @@ public class ShoppingCartAdapter extends XRecyclerView.Adapter<ShoppingCartAdapt
 
 
     private Context mContext;
+
+    private List<Cart> mCarts;
+
+    private IProductSelect mProductSelect;
+
+    private boolean selectAll;
+
+    public void setCarts(List<Cart> carts) {
+        this.mCarts = carts;
+        notifyDataSetChanged();
+    }
+
+    public interface IProductSelect{
+        void onProductedSelect(double price, boolean isSelectedAll);
+    }
+
+    public void selectedAll(boolean selected) {
+        if (mCarts != null) {
+            for (Cart cart : mCarts) {
+                if (selected) {
+                    cart.setSelected(true);
+                } else {
+                    cart.setSelected(false);
+                }
+            }
+            notifyDataSetChanged();
+            mProductSelect.onProductedSelect(compuatePrice(), selected);
+        }
+    }
+
+    public void setProductSelect(IProductSelect productSelect) {
+        mProductSelect = productSelect;
+    }
 
     public ShoppingCartAdapter(Context context) {
         mContext = context;
@@ -33,18 +72,72 @@ public class ShoppingCartAdapter extends XRecyclerView.Adapter<ShoppingCartAdapt
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        if (mCarts != null) {
+            Cart cart = mCarts.get(position);
+            holder.iv_cart_selected.setSelected(cart.isSelected());
 
+            // holder.iv_cart_fruit_img
+
+            holder.tv_cart_fruit_name.setText(cart.getName());
+            holder.tv_cart_price.setText(String.format(mContext.getString(R.string.cart_price), cart.getPrice()));
+            holder.tv_cart_spec.setText(String.format(mContext.getString(R.string.cart_spec), cart.getSpecification()));
+            holder.tv_cart_count.setText(String.format(mContext.getString(R.string.cart_count), cart.getCount()));
+
+            holder.iv_cart_selected.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    cart.setSelected(!cart.isSelected());
+                    mProductSelect.onProductedSelect(compuatePrice(), selectAll);
+                    notifyDataSetChanged();
+                }
+            });
+        }
     }
+
+    private double compuatePrice() {
+        if (mCarts != null) {
+            double totalPrice = 0;
+            selectAll = true;
+
+            for (Cart cart : mCarts) {
+                if (cart.isSelected()) {
+                    float price = cart.getPrice();
+                    int count = cart.getCount();
+                    double mul = ToolUtils.mul(price, count);
+                    totalPrice = ToolUtils.add(totalPrice, mul);
+                } else {
+                    selectAll = false;
+                }
+            }
+
+            return ToolUtils.scaleDouble(totalPrice, 2);
+        }
+        return 0.0;
+    }
+
 
     @Override
     public int getItemCount() {
-        return 10;
+        return mCarts != null ? mCarts.size() : 0;
     }
 
     class ViewHolder extends XRecyclerView.ViewHolder{
+        ImageView iv_cart_selected;
+        ImageView iv_cart_fruit_img;
+
+        TextView tv_cart_fruit_name;
+        TextView tv_cart_price;
+        TextView tv_cart_spec;
+        TextView tv_cart_count;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            iv_cart_selected = itemView.findViewById(R.id.iv_cart_selected);
+            iv_cart_fruit_img = itemView.findViewById(R.id.iv_cart_fruit_img);
+            tv_cart_fruit_name = itemView.findViewById(R.id.tv_cart_fruit_name);
+            tv_cart_price = itemView.findViewById(R.id.tv_cart_price);
+            tv_cart_spec = itemView.findViewById(R.id.tv_cart_spec);
+            tv_cart_count = itemView.findViewById(R.id.tv_cart_count);
         }
     }
 }
