2e9e8234a71497b4a402e06f66210b4789860156
teenyda <teenyda@gmail.com>
2020-11-07 13:49:18 +0800

    下订单

--- app/src/main/java/me/teenyda/fruit/model/classify/payment/PaymentAct.java
+++ app/src/main/java/me/teenyda/fruit/model/classify/payment/PaymentAct.java
@@ -3,6 +3,12 @@ package me.teenyda.fruit.model.classify.payment;
 import android.content.Context;
 import android.content.Intent;
 
+import java.util.Calendar;
+import java.util.Date;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import cn.iwgang.countdownview.CountdownView;
 import me.teenyda.fruit.R;
 import me.teenyda.fruit.common.mvp.MvpActivity;
 import me.teenyda.fruit.model.classify.payment.presenter.PaymentPresenter;
@@ -14,6 +20,9 @@ import me.teenyda.fruit.model.classify.payment.view.IPaymentView;
  * description:
  */
 public class PaymentAct extends MvpActivity<IPaymentView, PaymentPresenter> implements IPaymentView {
+
+    @BindView(R.id.cv_countdown)
+    CountdownView cv_countdown;
 
     public static void startActivity(Context context) {
         Intent intent = new Intent(context, PaymentAct.class);
@@ -37,6 +46,12 @@ public class PaymentAct extends MvpActivity<IPaymentView, PaymentPresenter> impl
     @Override
     protected void initView() {
         setTitleShow(true, "支付", false);
+        ButterKnife.bind(this);
+        Calendar c = Calendar.getInstance();
+        long start = c.getTimeInMillis();
+        c.add(Calendar.MINUTE, 10);
+        long end = c.getTimeInMillis();
+        cv_countdown.start(end - start);
     }
 
     @Override
